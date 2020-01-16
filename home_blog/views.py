11d2f7261e79ab49176f5046ed44e9d56426adc3
Romas <Romashka951@gmail.com>
2020-01-16 13:34:26 +0500

    Доработанна система отправки сообщений на e-mail

--- home_blog/views.py
+++ home_blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.core.mail import send_mail
+import pysnooper
 
 from .models import Post
 from .forms import EmailPostForm
@@ -14,14 +16,25 @@ class PostListView(ListView):
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-        else:
-            form = EmailPostForm()
-            return render(request, 'blog/post/share.html', {'post': post,
-                                                            'form': form})
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(
+                cd['name'], cd['email'], post.title
+            )
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(
+                post.title, post_url, cd['name'], cd['comments']
+            )
+            send_mail(subject, message, 'orangecat3324@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 def post_detail(request, year, month, day, post):
