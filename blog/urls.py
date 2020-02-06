d124edb005c943dd4d34a542012763ac72a14c02
Romas <Romashka951@gmail.com>
2020-02-06 15:08:39 +0500

    Добавленна карта сайта

--- blog/urls.py
+++ blog/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from home_blog.sitemaps import PostSitemap
+
+sitemaps = {'posts': PostSitemap,}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('home_blog.urls', namespace='home_blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
