e15bdeabad92c274eb74321c6913b86842b5ed16
Romas <Romashka951@gmail.com>
2020-02-07 16:01:42 +0500

    Добавленн полноценный поиск по постам и заголовкам

--- home_blog/views.py
+++ home_blog/views.py
@@ -4,10 +4,10 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-import pysnooper
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 
 
 class PostListView(ListView):
@@ -87,3 +87,24 @@ def post_detail(request, year, month, day, post):
         'comment_form': comment_form,
         'similar_posts': similar_posts
     })
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector(
+                'body', weight='B'
+            )
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(search=search_vector,
+                                            rank=SearchRank(
+                                                search_vector,
+                                                search_query)
+                                            ).filter(rank__gte=0.3).order_by('-rank')
+    return render(request, 'blog/post/search.html', {'form': form,
+                                                     'query': query,
+                                                     'results': results})
